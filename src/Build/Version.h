c723717e1cedc89b038b999b17fd16031c32aeb9
Thibault Jouan <tj@a13.fr>
2017-07-02 13:59:06 +0000

    Add FreeCAD version header
      Copied from:
    
      git://git.launchpad.net/~freecad-maintainers/+git/gitversioning

---
+++ src/Build/Version.h
@@ -0,0 +1,12 @@
+// Version Number
+#define FCVersionMajor "0"
+#define FCVersionMinor "17"
+#define FCVersionName  "Vulcan"
+// test: $Format:Hash (%H), Date: %ci$
+#define FCRevision      "11473 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2017/06/28 21:12:00"     //Date of highest committed revision
+#define FCRepositoryURL "git://github.com/FreeCAD/FreeCAD.git master"      //Repository URL of the working copy
+
+// Git relevant stuff
+#define FCRepositoryHash   "51c6ca053ab9417118b74dc6ae1763517c34f016"
+#define FCRepositoryBranch "master"
